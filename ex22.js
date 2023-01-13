7532ae7c40168ad77b458870267fb215ced1fdbf
BRam-coder <91925987+BRam-coder@users.noreply.github.com>
2023-01-13 08:34:34 -0500

    Add files via upload

---
+++ ex22.js
@@ -0,0 +1,27 @@
+let requestURL = "http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&tel={phone|format}&address={streetAddress}&city={city}&state={usState|abbr}&zip={zip}&pretty=true"
+
+fetch(requestURL)
+    .then((res) => { return res.json()})
+    .then((data) => {
+        console.log(data);
+        populateList(data);
+    });
+
+
+
+let studentData = document.getElementById("studentData");
+
+function populateList(obj){
+    let stringStudent = ''
+    obj.map((obj) => {
+    stringStudent = 
+        `<tr>
+            <td> ${obj.fname}</td> 
+            <td> ${obj.lname}</td>
+            <td> ${obj.address}</td>
+            <td> ${obj.city}</td>
+            <td> ${obj.tel}</td>
+        </tr>`
+    studentData.innerHTML += stringStudent;
+})};
+
